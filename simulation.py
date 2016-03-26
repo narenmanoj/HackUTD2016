cae9fe79872d3dc497a53e5e2c50fa0e2a939335
Naren S Manoj <manoj.narens@gmail.com>
2016-03-26 17:27:20 -0500

    function stubs created

--- simulation.py
+++ simulation.py
@@ -1,10 +1,37 @@
 class Simulation:
     i = 123
     programText = ""
+    currentPos = 0
 
-    def __init__(self):
+    'x86 box info'
+    rsi = 0
+    rdi = 0
+    rax = 0
+    rbp = 0
+    rsp = 0
+    rip = 0
+    r = list(12)
+
+    def __init__(self, inputText):
         self.i = 5
+        self.programText = inputText
 
     def loadProgramText(self, inputText):
-        programText = inputText
+        self.programText = inputText
 
+    def step(self):
+        self.currentPos += 1
+
+    def insert(self, instr):
+        'do something'
+
+    def reset(self):
+        'do something'
+
+    def remove(self):
+        'do something'
+
+    def skip(self):
+        'do something'
+
+    
